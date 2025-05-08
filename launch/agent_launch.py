9991e53ec16013674bb91b0323bbdc1b49594ab6
Mirek Burkon <mirek@phntm.io>
2025-05-08 16:29:51 -0700

    indent

--- launch/agent_launch.py
+++ launch/agent_launch.py
@@ -9,15 +9,15 @@ def generate_launch_description():
     agent_config = os.path.join(
         '/ros2_ws/',
         'phntm_agent_params.yaml'
-        )
+    )
     
     agent_node = Node(
-            package='phntm_agent',
-            executable='agent',
-            output='screen',
-            emulate_tty=True,
-            parameters=[agent_config]
-        )
+        package='phntm_agent',
+        executable='agent',
+        output='screen',
+        emulate_tty=True,
+        parameters=[agent_config]
+    )
 
     return LaunchDescription([
        agent_node
