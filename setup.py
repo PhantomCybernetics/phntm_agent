bf370cca60daf8962e4b50879bff8f21286f858f
Mirek Burkon <mirek@phntm.io>
2025-11-04 07:00:30 +0000

    removed python deps (was installing setuptools >= 80 and that causes problems)

--- setup.py
+++ setup.py
@@ -9,22 +9,19 @@ setup(
     version='0.0.2',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files.
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
-    install_requires=['setuptools', 'python-engineio', 'iwlib' ],
+    install_requires=['iwlib' ],
     zip_safe=True,
     maintainer='Mirek Burkon',
     maintainer_email='mirek@phntm.io',
     description='Monitoring Agent for the Phantom Bridge',
     license='MIT',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'agent = phntm_agent.agent:main',
         ],
-    },
+    }
 )
